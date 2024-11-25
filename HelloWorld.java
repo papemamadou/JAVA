cbdc9a20a211397f81d7c81e1e9948cc3384229b
papemamadou <papesy303001@gmail.com>
2024-11-24 16:28:34 -0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+   public class HelloWorld {
+        public void displayMessage() {
+            System.out.println("Hello World");
+        }
+    }
+    
+   public class MainClass {
+        public void(HelloWorld o2) 
+            }
+    
+        public static void main(String[] args) {
+            HelloWorld o2 = new HelloWorld();
+            MainClass o1 = new MainClass();
+            o1.m(o2);
+        }
+    }
+    
+
